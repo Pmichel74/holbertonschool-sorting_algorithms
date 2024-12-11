99634c4bce280c37d37e5918707cde0f94d088e4
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-11 17:08:07 +0100

    fix variable

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,38 +7,41 @@
  */
 void swap_ints(int *a, int *b)
 {
-	int tmp;
+	int temp;
 
-	tmp = *a;
+	temp = *a;
 	*a = *b;
-	*b = tmp;
+	*b = temp;
 }
 
 /**
  * bubble_sort - Sort an array of integers in ascending order.
- * @array: An array of integers to sort.
+ * @array: Array of integers to sort.
  * @size: The size of the array.
  *
- * Description: Prints the array after each swap.
+ * Description: Prints the array after each time you swap.
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, len = size;
-	bool bubbly = false;
+	bool flag = false;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (bubbly == false)
+	while (flag == false)/*false: continue tant qu'il y a des échanges*/
 	{
-		bubbly = true;
-		for (i = 0; i < len - 1; i++)
+		flag = true;/*si pas d'échange flag restera a true et fin de boucle*/
+		for (i = 0; i < len - 1; i++) /*-1 evite de dépasser limite du tableau*/
 		{
-			if (array[i] > array[i + 1])
+			if (array[i] > array[i + 1]) /* si élément actuel > que le suivant*/
 			{
 				swap_ints(array + i, array + i + 1);
+				/*fonction swap_ints pour swapper 2 élements*/
+				/*+ i 1er élément a échanger-->> + i + 1 élément suivant*/
 				print_array(array, size);
-				bubbly = false;
+				flag = false;
+				/* false: indique echange a eu lieu*/
 			}
 		}
 		len--;
